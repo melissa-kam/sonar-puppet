2b4a5fd8d49ab6f2fb17573f39f1668c94d3f09c
Melissa Kam <melissa.kam@rackspace.com>
2014-07-29 20:53:35 +0000

    Added plugins and postgres database.

--- modules/sonar/manifests/init.pp
+++ modules/sonar/manifests/init.pp
@@ -1,9 +1,47 @@
-class { 'postgresql::globals': }
-class { 'java': }
-class { 'maven::maven' : } ->
-class { 'sonarqube' :
-  version => '4.3.2',
-  user => 'sonar',
-  group => 'sonar',
-  service => 'sonar'
+$jdbc = {
+  url               => 'jdbc:postgresql://localhost/sonar',
+  username          => 'sonar',
+  password          => 'sonar',
 }
+
+
+class { 'java': }
+class { 'sonarqube':
+  version => '4.3.2',
+  jdbc => $jdbc,
+
+}
+
+
+sonarqube::plugin{'sonar-scm-activity':
+    groupid => 'org.codehaus.sonar-plugins.scm-activity',
+    artifactid => 'sonar-scm-activity-plugin',
+    version => '1.8',
+    notify => Service['sonar'],
+}
+
+sonarqube::plugin{'sonar-python-plugin':     
+    groupid => 'org.codehaus.sonar-plugins.python',
+    artifactid => 'sonar-python-plugin',
+    version => '1.3',
+    notify => Service['sonar'],
+}
+
+
+class { 'postgresql::server':
+    postgres_password => 'postgres',
+}
+
+postgresql::server::db{ 'sonar':
+    user => $jdbc['username'],
+    password => postgresql_password($jdbc['username'], $jdbc['password']),
+}
+
+class { 'python':
+    pip => true
+}
+
+python::pip { 'pylint':
+    pkgname => 'pylint'
+}
+
